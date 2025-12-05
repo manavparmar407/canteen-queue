847b98824f9d6842d510d2ebe13eacc8ad445b40
Manav Parmar <manavparmar0407@gmail.com>
2025-12-05 22:09:10 +0530

    Updated for Render deployment

--- app.py
+++ app.py
@@ -282,5 +282,9 @@ def update_order_status():
     return jsonify({"message": "Status updated"})
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
